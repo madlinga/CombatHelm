126b99f3c60f715d697a78621bb5665b808b0fbe
mitchell@adlington.net <mitchell@adlington.net>
2024-06-12 15:27:53 +0100

    feat: added the ability to enable and disable the addon dynamically in-game by using the /ch command. Also bug fixes to remove helm after certain scenarios such as death or loading screen.

--- CombatHelm/CombatHelm.lua
+++ CombatHelm/CombatHelm.lua
@@ -4,20 +4,42 @@ VERSION = 1.0
 -- Version and author message in console.
 print("CombatHelm: v" .. VERSION .. " | " .. "Author: " .. AUTHOR)
 
--- Helm does not show as default (on-load).
-ShowHelm(false)
+-- Creates a CLI /ch interface allowing users to configure if the addon is enabled dynamically.
+local function cliHandler(arg, editbox)
+    if arg == 'enable' then
+        enabled = true
+        print("CombatHelm enabled")
+    elseif arg == 'disable' then
+        enabled = false
+        print("CombatHelm disabled")
+    else
+        print("Invalid /ch command.\nOptions:\n    /ch enabled\n    /ch disabled")
+    end
+end
 
-local inCombat = UnitAffectingCombat("player")
-local holder = CreateFrame("Frame")
-holder:RegisterEvent("PLAYER_REGEN_ENABLED")
-holder:RegisterEvent("PLAYER_REGEN_DISABLED")
-holder:SetScript("OnEvent", function(self, event)
+SLASH_CH1 = '/ch'
+SlashCmdList["CH"] = cliHandler 
 
--- Shows helm when the player is in combat
-if UnitAffectingCombat("player") then
+-- Event Listener setup along with core logic.
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("ADDON_LOADED")
+frame:RegisterEvent("PLAYER_REGEN_ENABLED")
+frame:RegisterEvent("PLAYER_REGEN_DISABLED")
+frame:RegisterEvent("PLAYER_DEAD") -- Edge case for removing on death
+frame:RegisterEvent("PLAYER_ENTERING_WORLD") -- Edge case for removing helm after event such as BG, arena, dungeon etc.
+frame:SetScript("OnEvent", function(self, event, arg1)
+    if event == "ADDON_LOADED" and arg1 == "CombatHelm" then
+        if enabled == nil then 
+            enabled = true
+        end
+        print("CombatHelm loaded, enabled: ", enabled)
+    end
+
+    -- Shows helm when the player is in combat (if enabled)
+    if enabled and UnitAffectingCombat("player") then
         ShowHelm(true)
     else
         ShowHelm(false)
     end
-	
-end)
+end
+)
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CombatHelm v1.0
+# CombatHelm v1.1
 
 ![CombatHelm Icon](/assets/CombatHelm.png)
 
@@ -6,10 +6,14 @@
 
 ## Future Improvements
 
-- Add enabled toggle to allow users to configure whether to enable the addon.
 - Add features to enable show helm permanently in certain scenarios such as dungeons, raids, battlegrounds, arenas, duels, etc.
 
 ## Change log
+
+### v1.1
+
+Added the ability to enable and disable the addon dynamically in-game by using the /ch command - defaults to enabled if the var isn't found in memory (first time load).
+Added event listeners to detect when player dies or enters a loading screen to remove the helm, catering for bugs such as when a helm remained on after leaving a BG or releasing their spirit after death.
 
 ### v1.0
 
